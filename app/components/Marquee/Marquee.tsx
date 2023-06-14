1be02e5ea807a3d52ed8df702669ae573f12c228
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-06-14 11:35:11 +0200

    Improved marquee

--- app/components/Marquee/Marquee.tsx
+++ app/components/Marquee/Marquee.tsx
@@ -1,173 +1,38 @@
+// "use client";
 import clsx from "clsx";
+import { memo } from 'react';
 import FastMarquee from "react-fast-marquee";
+import { marqueeColors, marqueeTexts } from './Marquee.data';
+// import { useTheme } from "@wits/next-themes";
 
-const texts: string[][] = [
-  [
-    "Storybook",
-    "React",
-    "Redux",
-    "React Native",
-    "Expo",
-    "NativeBase",
-    "PWA's",
-    "NextJS",
-    "NextAuth",
-    "SSR/SSG",
-    "ISR",
-    "Tailwind",
-    "Angular",
-    "MUI",
-    "Material Design",
-    "SOLID",
-    "DRY",
-    "KPIs",
-    "Pimcore"
-  ],
-  [
-    "Typescript",
-    "UI/UX",
-    "RxJS",
-    "NodeJS/Express",
-    "Responsive",
-    "Atomic Design",
-    "FaunaDB",
-    "Shopify",
-    "GraphQL",
-    "Serverless",
-    "Cloud",
-    "Lambda",
-    "AWS",
-    "Saas",
-    "DynamoDB/Dynamoose",
-    "NoSQL",
-    "Debugging",
-    "Heroku",
-    "Github Packages",
-    "Git",
-    "Linux",
-  ],
-  [
-    "Lerna",
-    "Rollup",
-    "Monorepo",
-    "Turbopack",
-    "Vercel",
-    "Netlify",
-    "Azure",
-    "Yarn",
-    "NPM",
-    "DevOps",
-    "MySQL",
-    "MariaDB",
-    "Docker",
-    "Fetchmock",
-    "Postman",
-    "Insomnia",
-    "Swagger",
-    "Photoshop",
-    "Lightroom",
-    "Sonar Cube",
-    "Sentry"
-  ],
-  [
-    "Figma",
-    "AdobeXD",
-    "Justinmind",
-    "Notion",
-    "PNPM",
-    "Pure JS",
-    "HTML",
-    "SCSS",
-    "CSS3",
-    "UIKit",
-    "Twig",
-    "Bootstrap",
-    "MJML",
-    "Ruby",
-    "C#",
-    "PHP",
-    "Command Line",
-    "YAML",
-    "JSON",
-    "Agile",
-    "Scrum",
-  ],
-  [
-    "APIs",
-    "Android Studio",
-    "XCode Simulator",
-    "Flipper",
-    "Firebase",
-    "Cypress",
-    "Jest",
-    "Auth0",
-    "Miro",
-    "Zod",
-    "JWT",
-    "Stripe",
-    "ESlint",
-    "Prettier",
-    "Husky",
-    "Docker",
-    "Apollo",
-    "Axios",
-    "Formik",
-    "Confluence",
-    "Github",
-    "Bitbucket",
-  ],
-];
-
-const foo = texts.map((t) => {
-  return t.map((i) => {
-    return {
-      text: i,
-      _light: "#fff",
-      _dark: "#151515",
-    };
-  });
-});
-const marqueeContent: Array<
-  {
-    _light?: string;
-    _dark?: string;
-    text: string;
-  }[]
-> = [...foo];
 type MarqueeRowProps = {
   row: string[];
+  // theme: string | undefined;
 };
 const MarqueeRow = ({ row }: MarqueeRowProps) => {
   // @todo - support for theming
-  const colors: string[] = [
-    "#fff",
-    "#1795FF",
-    "#90D6FC",
-    "#45C2A7",
-    "#D7D7A0",
-    "#4C91D0",
-    "#ccc",
-    "#bc8670",
-    "#e3c042",
-    "#b463b5",
-  ];
+  // const { theme } = useTheme();
   return (
     <>
       {row.map((text, idx) => {
-        const idxColor = colors[idx % colors.length];
+        // const idxColor = theme === 'light' ? colors[idx % colors.length] : colorsDarkmode[idx % colors.length];
+        const idxColor = marqueeColors[idx % marqueeColors.length];
         return (
           <p
             key={idx}
             className={clsx(
-              "px-4 font-mono text-2xl md:text-4xl md:leading-relaxed",
-              // `text-[${colors[idx % colors.length]}]`,
+              // `text-[${idxColor}]`,
+              `text-[#000]`,
+              "marquee-text",
+              "px-4 text-4xl md:text-5xl !leading-loose",
               [1, 5].includes(idx)
                 ? "font-bold"
                 : [2, 4, 7].includes(idx)
                 ? "font-thin"
                 : "font-normal",
-              [3, 9].includes(idx) ? 'italic' : ''
+              [3, 9].includes(idx) ? "italic" : ""
             )}
+            // style={{ color: theme === 'light' ? '#000' : idxColor }}
             style={{ color: idxColor }}
           >
             {text}
@@ -178,28 +43,33 @@ const MarqueeRow = ({ row }: MarqueeRowProps) => {
   );
 };
 
-type MarqueeProps = {
-  // items?: string[][];
+type MarqueeProps = {};
+const Marquee = ({}: MarqueeProps) => {
+  // const { theme } = useTheme();
+
+  return (
+    <>
+      {marqueeTexts.map((row, index) => {
+        const speeds = [15, 30, 38, 8, 26];
+        return (
+          <FastMarquee
+            autoFill
+            pauseOnHover
+            key={index}
+            speed={speeds[index % speeds.length]}
+            direction={index % 2 === 0 ? "left" : "right"}
+            style={{
+              borderBottom: `1px solid ${
+                index + 1 >= marqueeTexts.length ? "transparent" : "#e3c042"
+              }`,
+            }}
+          >
+            <MarqueeRow row={row}></MarqueeRow>
+          </FastMarquee>
+        );
+      })}
+    </>
+  );
 };
-const Marquee = ({ }: MarqueeProps) => (
-  <>
-    {(texts).map((row, index) => {
-      const speeds = [15, 30, 38, 8, 26];
-      return (
-        <FastMarquee
-          autoFill
-          pauseOnHover
-          key={index}
-          speed={speeds[index % speeds.length]}
-          direction={index % 2 === 0 ? "left" : "right"}
-          style={{
-            borderBottom: `1px solid ${index + 1 >= texts.length ? 'transparent' : '#e3c042'}`
-          }}
-        >
-          <MarqueeRow row={row}></MarqueeRow>
-        </FastMarquee>
-      );
-    })}
-  </>
-);
+
 export default Marquee;
