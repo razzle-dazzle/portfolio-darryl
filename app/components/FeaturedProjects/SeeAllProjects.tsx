4b2ca778237bc938fb4d33de09379e3767066c5d
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-09-06 17:36:30 +0200

    Use large button

--- app/components/FeaturedProjects/SeeAllProjects.tsx
+++ app/components/FeaturedProjects/SeeAllProjects.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 import { useRouter } from "next/navigation";
-import { NAV_ITEMS } from 'app/constants';
-import Button from '../Button';
+import { NAV_ITEMS } from "app/constants";
+import Button from "../Button";
 
 export default function SeeAllProjects() {
   const router = useRouter();
 
   return (
-    <Button icon onClick={() => router.push(NAV_ITEMS.projects.path)}>
+    <Button
+      icon
+      onClick={() => router.push(NAV_ITEMS.projects.path)}
+      size="large"
+    >
       See all projects
     </Button>
-  )
+  );
 }
